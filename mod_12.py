5ea680fd2d6a3200280095c68b368f66680784cb
AmilisQA <efremova.arbf@mail.ru>
2021-04-21 21:48:53 +0300

    message

---
+++ mod_12.py
@@ -0,0 +1,11 @@
+
+money = int(input("Введите сумму вклада = "))
+
+#словарь per_cent содержит данные по процентным ставкам вклада
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+per = (list(map(float,per_cent.values())))
+deposit = list(map(int,[per[0]*money/100, per[1]*money/100, per[2]*money/100, per[3]*money/100]))
+
+print('Сумма процентов по вкладу: ', deposit)
+print('Максимальная сумма от процентов по вкладу за год =', max(deposit))
